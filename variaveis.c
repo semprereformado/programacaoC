a8d839dad716bbaa9c67905007be14558705d858
Cleber Carvalho <140116304+semprereformado@users.noreply.github.com>
2025-05-15 18:44:14 +0000

    Atualiza o arquivo 'variaveis.c'.

--- variaveis.c
+++ variaveis.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int idade;
-    int quantidade;
-    float altura;
-    double peso;
-    char letra;
-    char nome[20];
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = 'cleber';
     return 0;
 
 }
